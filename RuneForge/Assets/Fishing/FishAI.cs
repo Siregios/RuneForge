a8ef990a7e21b6945f81b3dc7143408af6c380cc
pgtruong <petergtruong@gmail.com>
2016-10-01 10:02:22 -0700

    Fish can randomly move.

--- RuneForge/Assets/Fishing/FishAI.cs
+++ RuneForge/Assets/Fishing/FishAI.cs
@@ -3,35 +3,77 @@ using System.Collections;
 
 public class FishAI : MonoBehaviour {
 
-    public float speed = 1;
-    private float pause = 20;
+    //Controls speed of fish and the amount of time the fish pauses.
+    public float speed = 5f;
+    private float pause = 1.5f;
+
+    //Records the last pause fish had and variable for periods between pauses.
     private float lastPause;
+    public float pauseBetween = 2;
+
+    //Makes fish move for x amount of frames.
+    private int frameDuration = 0;
+    private int amountOfFrames = 60;
+    private bool direction = true;
     Vector3 pos;
-    int rand = 0;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         lastPause = Time.time;
     }
-	
-	// Update is called once per frame
-	void Update () {
-        rand = Random.Range(0, 2);
-        if (rand == 0 && lastPause > Time.time)
-        {
-            MovementAI();
-        }
-        else
-        {
-            lastPause = Time.time + pause;
-        }
+
+    // Update is called once per frame
+    void Update() {
+        MovementAI();
     }
 
+
+    //Moves the fish
     private void MovementAI()
-    {
-        transform.position += transform.up * speed * Time.deltaTime;
-      
-    }
+    {        
+        Debug.Log(frameDuration);
+        //checks for last pause and if fish is still in movement.
+        if (lastPause < Time.time && frameDuration == 0)
+        {
+            //95% chance movement or 5% chance pause.
+            if (Random.Range(1, 101) <= 95)
+            {
+                //Moves for "amountOfFrames"
+                frameDuration = amountOfFrames;
+                //50% chance up or down
+                if (Random.Range(1, 101) <= 51)
+                {
+                    transform.position += transform.up * speed * Time.deltaTime;
+                    direction = true;
+                }
+                else
+                {
+                    transform.position -= transform.up * speed * Time.deltaTime;
+                    direction = false;
+                }
+            }
 
+            //Pause fish for "pause" seconds
+            else if (Time.time - lastPause > pauseBetween)
+            {
+                lastPause = Time.time + pause;
+            }
+        }
+        //If frames not complete, fish will continue to move that direction
+        else if (frameDuration != 0)
+        {
+            frameDuration--;
+            if (direction)
+            {
+                transform.position += transform.up * speed * Time.deltaTime;
+            }
+            else
+            {
+                transform.position -= transform.up * speed * Time.deltaTime;
+            }
+        }
+        
+    }
 }
+
