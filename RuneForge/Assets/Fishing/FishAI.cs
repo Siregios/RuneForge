2e82ccd85653b0fad7ce224443e41ef3ae530736
pgtruong <petergtruong@gmail.com>
2016-10-01 00:13:23 -0700

    Fishing pt1
    This is trash tho.

---
+++ RuneForge/Assets/Fishing/FishAI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishAI : MonoBehaviour {
+
+    public float speed = 1;
+    private float pause = 20;
+    private float lastPause;
+    Vector3 pos;
+    int rand = 0;
+
+
+	// Use this for initialization
+	void Start () {
+        lastPause = Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rand = Random.Range(0, 2);
+        if (rand == 0 && lastPause > Time.time)
+        {
+            MovementAI();
+        }
+        else
+        {
+            lastPause = Time.time + pause;
+        }
+    }
+
+    private void MovementAI()
+    {
+        transform.position += transform.up * speed * Time.deltaTime;
+      
+    }
+
+}
