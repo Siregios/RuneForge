160c605f4f6a5b87823a5befe68f5f950cf9a755
eaho1 <eaho555@gmail.com>
2016-10-01 16:48:14 -0700

    Inventory pt2 & Shop pt1
    Edited the inventory to separate materials and runes into lists.
    Added basic functionality to the shop such as creating pages
    representing the inventory.

---
+++ RuneForge/Assets/UI/Shop/Inventory.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class Inventory {
+    public static Dictionary<string, int> inventoryDict = new Dictionary<string, int>();
+    public static List<string> materialList, runeList;
+
+    static Inventory()
+    {
+        //Money
+        CreateItem("Money");
+
+        //Basic Materials
+        materialList = new List<string>() { "OakWood", "CopperBar", "Bone", "Scale", "Clay", "Chalk" };
+        foreach (string material in materialList)
+            CreateItem(material);
+
+        //Runes
+        runeList = new List<string>() { "Fire", "Water", "Air", "Earth", "MagnetPlus", "MagnetMinus"};
+        foreach (string rune in runeList)
+            CreateRune(rune);
+    }
+
+    public static void AddItem(string item)
+    {
+        inventoryDict[item]++;
+    }
+
+    public static void AddItem(string item, int count)
+    {
+        inventoryDict[item] = count;
+    }
+
+    public static int GetItemCount(string item)
+    {
+        return inventoryDict[item];
+    }
+
+    static void CreateItem(string item)
+    {
+        inventoryDict.Add(item, 0);
+    }
+
+    static void CreateRune(string rune)
+    {
+        CreateItem(rune + "S");
+        CreateItem(rune + "A");
+        CreateItem(rune + "B");
+        CreateItem(rune + "C");
+    }
+}
---
+++ RuneForge/Assets/UI/Shop/ShopUI.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShopUI : MonoBehaviour {
+
+    public GameObject RuneButton;
+    GameObject actionPanel;
+    GameObject itemsPanel;
+
+    void Awake()
+    {
+        actionPanel = this.transform.FindChild("ActionPanel").gameObject;
+        itemsPanel = this.transform.FindChild("ItemsPanel").gameObject;
+    }
+
+    public void DisplayRunePage(int page)
+    {
+        DisplayRune(page);
+        DisplayRune(page + 1);
+        DisplayRune(page + 2);
+    }
+
+    void DisplayRune(int runeIndex)
+    {
+        string runeType = Inventory.runeList[runeIndex];
+        int yPos = 150 - (runeIndex % 3 * 120);
+        foreach (char rank in "SABC")
+        {
+            GameObject newRuneButton = Instantiate(RuneButton, itemsPanel.transform.position, Quaternion.identity) as GameObject;
+            newRuneButton.transform.SetParent(itemsPanel.transform);
+            newRuneButton.transform.localScale = Vector3.one;
+            newRuneButton.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, yPos, 0);
+            yPos -= 30;
+
+            string rune = runeType + rank;
+            newRuneButton.transform.FindChild("Text").GetComponent<Text>().text = runeType;
+            newRuneButton.transform.FindChild("Count").GetComponent<Text>().text = "x" + Inventory.GetItemCount(rune).ToString();
+        }
+    }
+
+    public void DisplayMaterialsPage(int page)
+    {
+        DisplayRune(page);
+        DisplayRune(page + 1);
+        DisplayRune(page + 2);
+    }
+}
