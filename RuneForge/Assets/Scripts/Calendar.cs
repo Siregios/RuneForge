e3b9992f5ff644f55d59d743e49efe70576ef3e4
eaho1 <eaho555@gmail.com>
2016-10-04 14:24:29 -0700

    Calendar Prototype

---
+++ RuneForge/Assets/Scripts/Calendar.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Calendar : MonoBehaviour {
+    public int day = 1;
+    public int season = 1;
+    public float time;
+    public float startTime;
+    public float dayLength;
+    // Use this for initialization
+    void Start () {
+        startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time = Time.time;
+        if(time - startTime > dayLength)
+        {
+            startTime = Time.time;
+            day++;
+        }
+	    if(day > 30)
+        {
+            day = 1;
+            if(season > 4)
+                season = 1;
+            else
+            {
+                season++;
+            }
+        }
+	}
+}
