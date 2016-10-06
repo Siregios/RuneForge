3dce2e7dff4cdb3043cccbb05976ed7836ddc013
Christopher Peterson <cjpeter1@uci.edu>
2016-10-06 00:14:13 -0700

    Walking Animation and Animator Control Script

---
+++ RuneForge/Assets/PlayerAnimController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimController : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("walk", true);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("walk", true);
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetBool("walk", false);
+        }
+    }
+}
