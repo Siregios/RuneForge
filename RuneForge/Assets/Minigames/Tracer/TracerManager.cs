9369bbd6d0c9b9117dd86e70efd6ebad5f244747
Edwin Ho <eaho555@gmail.com>
2016-10-16 01:57:19 -0700

    Tweaked Tracer
    Tracer minigame now keeps track of combo score and also leaves a trail
    from the mouse when you collect a dot for 5 seconds.

--- RuneForge/Assets/Minigames/Tracer/TracerManager.cs
+++ RuneForge/Assets/Minigames/Tracer/TracerManager.cs
@@ -7,19 +7,30 @@ public class TracerManager : MonoBehaviour {
     RandomSpawner spawner;
 
     public GameObject lineRenderer = null;
+    public GameObject trailRenderer = null;
     public float spawnInterval = 1f;
+
+    [HideInInspector]
+    public int score = 0;
+
+    GameObject currentTrail = null;
     float cooldown;
 
     List<GameObject> lineList = new List<GameObject>();
     Vector3 previousDotLoc = Vector3.back;
 
+    //For testing purposes only
+    public bool trailStays = false;
+
     void Awake()
     {
         spawner = this.GetComponent<RandomSpawner>();
+        Cursor.visible = false;
     }
 
     void Start()
     {
+        CreateNewTrail();
         cooldown = spawnInterval;
     }
 
@@ -31,20 +42,30 @@ public class TracerManager : MonoBehaviour {
         {
             SpawnDot();
         }
+
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currentTrail.transform.position = mousePos;        
     }
 
     public void DotTouched(GameObject dot)
     {
+        score++;
+        if (trailStays)
+            DropTrail();
+
         Vector3 newLoc = dot.transform.position;
-        if (previousDotLoc != Vector3.back)
-        {
-            GameObject newLine = Instantiate(lineRenderer) as GameObject;
-            LineRenderer line = newLine.GetComponent<LineRenderer>();
-            line.SetPosition(0, previousDotLoc);
-            line.SetPosition(1, newLoc);
-            line.sortingLayerName = "Background";
-            lineList.Add(newLine);
-        }
+        
+        ///Straight lines between 2 points below
+
+        //if (previousDotLoc != Vector3.back)
+        //{
+        //    GameObject newLine = Instantiate(lineRenderer) as GameObject;
+        //    LineRenderer line = newLine.GetComponent<LineRenderer>();
+        //    line.SetPosition(0, previousDotLoc);
+        //    line.SetPosition(1, newLoc);
+        //    line.sortingLayerName = "Background";
+        //    lineList.Add(newLine);
+        //}
         //else
         //{
         //    GameObject newLine = Instantiate(lineRenderer) as GameObject;
@@ -53,12 +74,14 @@ public class TracerManager : MonoBehaviour {
         //    line.SetPosition(1, newLoc + new Vector3(.1f, .1f, 0));
         //    lineList.Add(newLine);
         //}
+
         previousDotLoc = newLoc;
         SpawnDot();
     }
 
     public void DotMissed(GameObject dot)
     {
+        score = 0;
         foreach (GameObject line in lineList){
             Destroy(line);
         }
@@ -70,5 +93,19 @@ public class TracerManager : MonoBehaviour {
     {
         spawner.SpawnObject();
         cooldown = spawnInterval;
+    }
+
+    void CreateNewTrail()
+    {
+        currentTrail = (GameObject)Instantiate(trailRenderer);
+    }
+
+    void DropTrail()
+    {
+        TrailRenderer trailRend = currentTrail.GetComponent<TrailRenderer>();
+        trailRend.time = 5;
+        trailRend.material.SetColor("_TintColor", Color.cyan);
+        trailRend.autodestruct = true;
+        CreateNewTrail();
     }
 }
---
+++ RuneForge/Assets/Minigames/Tracer/TracerUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TracerUI : MonoBehaviour {
+
+    TracerManager GM;
+    Text scoreText;
+
+    void Awake()
+    {
+        GM = GameObject.Find("GameManager").GetComponent<TracerManager>();
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        scoreText.text = "x" + GM.score;
+    }
+}
