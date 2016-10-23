7d0f404817459c4f5e7cb918a26569df4ef7add6
pgtruong <petergtruong@gmail.com>
2016-10-22 18:57:19 -0700

    Rhythm Game Pt 3
    Completed basic mechanics, mapping is all thats left and pretty
    animation stuff

---
+++ RuneForge/Assets/Minigames/Rhythm/Notes/NoteTracker.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteTracker : MonoBehaviour {
+    public int accuracy = 0;
+	
+	void Start () {
+        if (transform.position == new Vector3(0, 6, 0))
+        {            
+        }
+	}
+	
+	
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, GameObject.Find("center").transform.position, 2 * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag != "Note")
+            accuracy++;
+        if (accuracy == 3)
+            Destroy(gameObject);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKeyDown(KeyCode.W) && other.gameObject.name == "w_key")
+        {
+            Debug.Log(accuracy);
+            Destroy(gameObject);
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && other.gameObject.name == "s_key")
+        {
+            Debug.Log(accuracy);
+            Destroy(gameObject);
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && other.gameObject.name == "a_key")
+        {
+            Debug.Log(accuracy);
+            Destroy(gameObject);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && other.gameObject.name == "d_key")
+        {
+            Debug.Log(accuracy);
+            Destroy(gameObject);
+        }
+    }
+}
--- RuneForge/Assets/Minigames/Rhythm/RandomSpawnNote.cs
+++ RuneForge/Assets/Minigames/Rhythm/RandomSpawnNote.cs
@@ -1,16 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using SynchronizerData;
 
 public class RandomSpawnNote : MonoBehaviour {
     public GameObject[] spawnObject;
+    int randomInt = 1;
+    private BeatObserver beatObserver;
+    private int beatCounter;
 
-	void Start () {
-
-	}
+    void Start () {
+        beatObserver = GetComponent<BeatObserver>();
+        beatCounter = 0;
+        Instantiate(spawnObject[0], spawnObject[0].transform.position, Quaternion.identity);
+    }
 		
 	void Update () {
-	
+        if (beatObserver.beatMask == BeatType.OnBeat)
+        {
+            randomInt = Random.Range(1, 5);
+            if (randomInt == 1)            
+                Instantiate(spawnObject[0], spawnObject[0].transform.position, Quaternion.identity);                
+            else if (randomInt == 2)
+                Instantiate(spawnObject[1], spawnObject[1].transform.position, Quaternion.identity);
+            else if (randomInt == 3)
+                Instantiate(spawnObject[2], spawnObject[2].transform.position, Quaternion.identity);
+            else if (randomInt == 4)
+                Instantiate(spawnObject[3], spawnObject[3].transform.position, Quaternion.identity);
+        }
 	}
-
-
 }
