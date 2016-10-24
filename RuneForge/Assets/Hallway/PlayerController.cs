5ae6f52a0ddeeaeb6696dc8210a317150500e07a
Christopher Peterson <cjpeter1@uci.edu>
2016-10-24 14:17:08 -0700

    Added Store Background
    Added Monami's backgrounds for the store scene, added colliders for the
    floors and walls and added a door to the far left. Detatched the camera
    from the player.

---
+++ RuneForge/Assets/Hallway/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject door;
+    public float speed = 1f;
+
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            //gameObject.transform.position = new Vector3(gameObject.transform.position.x - .1f, gameObject.transform.position.y, gameObject.transform.position.z);
+            //this.transform.Translate(-speed * Time.deltaTime, 0, 0);
+            this.GetComponent<Rigidbody2D>().MovePosition(this.transform.position + new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //gameObject.transform.position = new Vector3(gameObject.transform.position.x + .1f, gameObject.transform.position.y, gameObject.transform.position.z);
+            //this.transform.Translate(speed * Time.deltaTime, 0, 0);
+            this.GetComponent<Rigidbody2D>().MovePosition(this.transform.position + new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if(door != null)
+            {
+                //gameObject.transform.position = new Vector3(door.GetComponent<Door>().connected.transform.position.x, door.GetComponent<Door>().connected.transform.position.y, door.GetComponent<Door>().connected.transform.position.z);
+                this.transform.position = door.GetComponent<Door>().connected.transform.position;
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Door"))
+            door = col.gameObject;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Door"))
+            door = null;
+    }
+}
