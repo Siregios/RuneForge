0ba5bad75713b36b8c0b82a6f363917b2f3e88fb
Christopher Peterson <cjpeter1@uci.edu>
2016-10-24 14:55:28 -0700

    Fixed up some scene stuff

--- RuneForge/Assets/Hallway/Door.cs
+++ RuneForge/Assets/Hallway/Door.cs
@@ -5,13 +5,4 @@ public class Door : MonoBehaviour {
 
     public GameObject connected;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
