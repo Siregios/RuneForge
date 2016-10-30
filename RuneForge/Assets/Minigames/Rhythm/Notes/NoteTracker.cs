edc59eb7c94a46d6d5388f3769bdf246f82a3229
pgtruong <petergtruong@gmail.com>
2016-10-29 20:08:26 -0700

    Somewhat Polished Rhythm Game
    It works well, mapping a beat works well and it goes on beat. Fixed an
    issue with keys not registering with getkeydown.  All thats left is to
    fix issues when fast beats spawn and you can kill two with one stone.
    
    Parameters I will add for scoring later:
    Accuracy 0 = Good
    Accuracy 1 = Great
    Accuracy 2 = Perfect
    Accuracy 3 = Miss

--- RuneForge/Assets/Minigames/Rhythm/Notes/NoteTracker.cs
+++ RuneForge/Assets/Minigames/Rhythm/Notes/NoteTracker.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class NoteTracker : MonoBehaviour {
     public int accuracy = 0;
+    public float speed = 3.5f;
 
 	void Start () {
         if (transform.position == new Vector3(0, 6, 0))
@@ -11,39 +12,41 @@ public class NoteTracker : MonoBehaviour {
 	}
 	
 	
-	void Update () {   
-        transform.position = Vector3.MoveTowards(transform.position, GameObject.Find("center").transform.position, 3.5f * Time.deltaTime);
+	void Update () {
+
+        transform.position = Vector3.MoveTowards(transform.position, GameObject.Find("center").transform.position, speed * Time.deltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag != "Note" && other.gameObject.name != "hitbox")
             accuracy++;
+
         if (accuracy == 3)
         {
             Debug.Log("Miss!");
             Destroy(gameObject);
-        }
+        }        
     }
 
     void OnTriggerStay2D(Collider2D other)
     {
-        if (Input.GetKeyDown(KeyCode.W) && other.gameObject.name == "w_key" && accuracy != 3)
+        if (Input.GetKey(KeyCode.W) && other.gameObject.name == "w_key" && accuracy != 3)
         {
             Debug.Log(accuracy);
             Destroy(gameObject);
         }
-        else if (Input.GetKeyDown(KeyCode.S) && other.gameObject.name == "s_key" && accuracy != 3)
+        else if (Input.GetKey(KeyCode.S) && other.gameObject.name == "s_key" && accuracy != 3)
         {
             Debug.Log(accuracy);
             Destroy(gameObject);
         }
-        else if (Input.GetKeyDown(KeyCode.A) && other.gameObject.name == "a_key" && accuracy != 3)
+        else if (Input.GetKey(KeyCode.A) && other.gameObject.name == "a_key" && accuracy != 3)
         {
             Debug.Log(accuracy);
             Destroy(gameObject);
         }
-        else if (Input.GetKeyDown(KeyCode.D) && other.gameObject.name == "d_key" && accuracy != 3)
+        else if (Input.GetKey(KeyCode.D) && other.gameObject.name == "d_key" && accuracy != 3)
         {
             Debug.Log(accuracy);
             Destroy(gameObject);
