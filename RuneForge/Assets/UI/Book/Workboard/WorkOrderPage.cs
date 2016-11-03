cf7fcc014977406bec30131228bc16b67a0fa0d1
Edwin Ho <eaho1@uci.edu>
2016-11-02 21:24:37 -0700

    Pushing some intermediate Workboard changes.
    Nothing really, just pushing what I have on the lab computer so I can
    work on it at home.

---
+++ RuneForge/Assets/UI/Book/Workboard/WorkOrderPage.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WorkOrderPage : MonoBehaviour { 
+    public GameObject minigameLine; //A panel with a check icon (Image), name of the minigame (Text), and score (Text)
+    WorkOrder order;
+    Text orderText;
+    Image orderIcon;
+
+    
+}
