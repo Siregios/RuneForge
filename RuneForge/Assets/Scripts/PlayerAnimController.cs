3df6db3e20b294379fbfc70a51a8a5de12c03577
pgtruong <petergtruong@gmail.com>
2016-11-05 13:02:46 -0700

    Left Click Functionality
    You can left click to move!

--- RuneForge/Assets/Scripts/PlayerAnimController.cs
+++ RuneForge/Assets/Scripts/PlayerAnimController.cs
@@ -4,18 +4,22 @@ using System.Collections;
 public class PlayerAnimController : MonoBehaviour {
 
     Animator anim;
+    PlayerController player;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
+        player = transform.parent.gameObject.GetComponent<PlayerController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //Edwin holy moly
         if (GetComponent<SpriteRenderer>().flipX)
             GetComponentInParent<BoxCollider2D>().offset = new Vector2(Mathf.Abs(GetComponentInParent<BoxCollider2D>().offset.x) , GetComponentInParent<BoxCollider2D>().offset.y);
         else
             GetComponentInParent<BoxCollider2D>().offset = new Vector2(-1 * Mathf.Abs(GetComponentInParent<BoxCollider2D>().offset.x), GetComponentInParent<BoxCollider2D>().offset.y);
+
         if (Input.GetKey(KeyCode.A))
         {
             anim.SetBool("walk", true);
@@ -26,7 +30,16 @@ public class PlayerAnimController : MonoBehaviour {
             anim.SetBool("walk", true);
             GetComponent<SpriteRenderer>().flipX = true;
         }
-        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+
+        if (player.mouseClick)
+        {
+            anim.SetBool("walk", true);
+            if (player.playerPosition.x - player.targetClick.x < 0)
+                GetComponent<SpriteRenderer>().flipX = true;
+            else
+                GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) || (!player.mouseClick && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)))
         {
             anim.SetBool("walk", false);
         }
--- RuneForge/Assets/Store/PlayerController.cs
+++ RuneForge/Assets/Store/PlayerController.cs
@@ -6,8 +6,11 @@ public class PlayerController : MonoBehaviour {
     public float speed = 1f;
 
     Rigidbody2D rigidBody;
-
     private Door door;
+    public Vector3 playerPosition;
+    //For left click movement    
+    public bool mouseClick = false;
+    public Vector3 targetClick;
 
     void Awake()
     {
@@ -15,24 +18,58 @@ public class PlayerController : MonoBehaviour {
     }
 
 	void Update () {
-
-        if (Input.GetKey(KeyCode.A))
+        //Update the player position erryday
+        playerPosition = transform.position;
+        if (door != null)
         {
-            rigidBody.MovePosition(this.transform.position + new Vector3(-speed * Time.deltaTime, rigidBody.velocity.y * Time.deltaTime, 0));
+            MasterGameManager.instance.sceneManager.LoadScene(door.nextScene);
+        }
+
+        //If key is A or D, move player that direction
+        //mouseclick false and velocity zero in case player tries to click and use keys at the same time, keys take priority.
+        if (Input.GetKey(KeyCode.A))
+        {            
+            rigidBody.MovePosition(playerPosition + new Vector3(-speed * Time.deltaTime, 0, 0));
+            rigidBody.velocity = Vector3.zero;
+            mouseClick = false;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            rigidBody.MovePosition(this.transform.position + new Vector3(speed * Time.deltaTime, rigidBody.velocity.y * Time.deltaTime, 0));
+            rigidBody.MovePosition(playerPosition + new Vector3(speed * Time.deltaTime, 0, 0));
+            rigidBody.velocity = Vector3.zero;
+            mouseClick = false;
         }
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            if (door != null)
+            targetClick = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetClick = new Vector3(targetClick.x, playerPosition.y, playerPosition.z);
+            mouseClick = true;
+        }
+        //if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //    if (door != null)
+        //    {
+        //        //Bring up UI to select different rooms in the house
+        //        MasterGameManager.instance.sceneManager.LoadScene(door.nextScene);
+        //        //Vector2 nextLocation = door.nextDoor.transform.position;
+        //        //rigidBody.MovePosition(nextLocation);
+        //        //Camera.main.transform.position = new Vector3(door.nextDoor.cameraLocation.x, door.nextDoor.cameraLocation.y, -10);
+        //    }
+        //}
+    }
+
+    void FixedUpdate()
+    {
+        //If mouse click, move rigidbody 
+        if (mouseClick)
+        {
+            Vector3 direction = (targetClick - playerPosition).normalized * speed;
+            rigidBody.velocity = direction;
+            if (Mathf.Abs(playerPosition.x - targetClick.x) < 2f)
             {
-                //Bring up UI to select different rooms in the house
-                MasterGameManager.instance.sceneManager.LoadScene(door.nextScene);
-                //Vector2 nextLocation = door.nextDoor.transform.position;
-                //rigidBody.MovePosition(nextLocation);
-                //Camera.main.transform.position = new Vector3(door.nextDoor.cameraLocation.x, door.nextDoor.cameraLocation.y, -10);
+                rigidBody.velocity = Vector3.zero;
+                playerPosition = targetClick;
+                mouseClick = false;
             }
         }
     }
@@ -48,4 +85,19 @@ public class PlayerController : MonoBehaviour {
         if (col.CompareTag("Door"))
             door = null;
     }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "VerticalBounds")
+        {            
+            mouseClick = false;
+            rigidBody.velocity = Vector3.zero;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        OnCollisionEnter2D(other);
+    }    
+    
 }
