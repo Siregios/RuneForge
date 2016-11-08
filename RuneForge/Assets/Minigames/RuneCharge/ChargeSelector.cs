b6a0ba7026ad8c1d9bf9f53b84da12333b7de23c
efrenaguilar95 <efrenaguilar95@hotmail.com>
2016-11-08 08:50:51 -0800

    RuneChargePt3
    Fixed a few logic errors as well as implmented a timer between missses
    so I could reset after a miss instead of alllowing the player to button
    spam the whole game.
    
    Also included some dummy art to try and get a feel for the aesthetics.

--- RuneForge/Assets/Minigames/RuneCharge/ChargeSelector.cs
+++ RuneForge/Assets/Minigames/RuneCharge/ChargeSelector.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class ChargeSelector : MonoBehaviour {
 
-    private bool inSelection = false;   //Is the pointer in the green?
+    private bool inSelection = false;   //Is the marker in the green?
     private bool buttonMash = false;    //True if game is in button mash mode
+    //may replace later
+    private bool isPlaying = true;      //False during animations and wait times
 
+       
     private int mashCount = 0;
     private float lastSpeed;
     public float startXPos = -6.5f;
@@ -17,19 +20,30 @@ public class ChargeSelector : MonoBehaviour {
     private TargetMovment target;
     private movementAI movement;
 
+    //These will all be replaced with an animation or particle affect at some point
+    private SpriteRenderer[] lightning;
+
 	// Use this for initialization
 	void Start () {
 
         target = GameObject.Find("Target").GetComponent<TargetMovment>();
         movement = this.GetComponent<movementAI>();
-	
-	}
+
+        //to be replaced
+        lightning = new SpriteRenderer[3];
+        lightning[0] = GameObject.Find("LIGHTNING").GetComponent<SpriteRenderer>();
+        lightning[1] = GameObject.Find("MORE LIGHTNING").GetComponent<SpriteRenderer>();
+        lightning[2] = GameObject.Find("EVEN MORE LIGHTNING").GetComponent<SpriteRenderer>();
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (selectButtonDown())
+        if (selectButtonDown() && isPlaying)
         {
+
             if (buttonMash)
                 mashUp();
             else
@@ -57,38 +71,83 @@ public class ChargeSelector : MonoBehaviour {
         return false;
     }
 
-    //Reset position of pointer. If scored, add to score
+    //Reset position of marker. If scored, add to score
     void checkForScore()
     {
+        stopMarker();
+        lightning[0].enabled = true;
         if (inSelection)
         {
             score++;
-            if (movement.speed < maxSpeed)
-            {
-                lastSpeed = movement.speed;
-                movement.maxSpeed = 0;
-                movement.minSpeed = 0;
-                movement.speed = 0;
-            }
+            Debug.Log("RIDE THE LIGHTNING!");
             buttonMash = true;
         }
-        //reset pointer on button push
+        else
+        {
+            Debug.Log("MISS!");
+            //to be replaced
+            StartCoroutine(waitForLightning());
+        }
+
 
     }
 
     void mashUp()
     {
-        Debug.Log(mashCount);
+        //Debug.Log(mashCount);
         mashCount++;
+
+        //to be replaced
+        if (((float)mashCount / 25) * 100 > 50)
+            lightning[1].enabled = true;
+        if (((float)mashCount / 25) * 100 > 75)
+            lightning[2].enabled = true;
+
         if (mashCount == 25)    //After some number/time of button mashing, reset and increase speed
         {
             buttonMash = false;
+            mashCount = 0;
+            StartCoroutine(waitForLightning());
+        }
+    }
+
+    //stop the marker's movement and save its speed
+    void stopMarker()
+    {
+        lastSpeed = movement.speed;
+        movement.maxSpeed = 0;
+        movement.minSpeed = 0;
+        movement.speed = 0;
+    }
+
+    //resets the marker and sets a new positon for the target
+    void resetMarker()
+    {
+        transform.position = new Vector2(startXPos, startYPos);
+        if(lastSpeed + speedIncrement <= maxSpeed)
+        {
             movement.maxSpeed = lastSpeed + speedIncrement;
             movement.minSpeed = lastSpeed + speedIncrement;
             movement.speed = lastSpeed + speedIncrement;
-            target.changePosition();
-            mashCount = 0;
-            transform.position = new Vector2(startXPos, startYPos);
         }
+        else
+        {
+            movement.maxSpeed = lastSpeed;
+            movement.minSpeed = lastSpeed;
+            movement.speed = lastSpeed;
+        }
+        target.changePosition();
+    }
+
+    //may be replaced
+    IEnumerator waitForLightning()
+    {
+        isPlaying = false;
+        yield return new WaitForSeconds(1f);
+        isPlaying = true;
+        //to be replaced
+        for (int i = 0; i < 3; i++)
+            lightning[i].enabled = false;
+        resetMarker();
     }
 }
