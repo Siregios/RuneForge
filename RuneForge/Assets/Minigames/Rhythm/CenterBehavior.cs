729ef56557cdd9f327562bf90fa0e1e6acdbaf28
pgtruong <petergtruong@gmail.com>
2016-11-10 14:10:59 -0800

    Added beat synchronzation need to fix
    also fixed small hook thing

---
+++ RuneForge/Assets/Minigames/Rhythm/CenterBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using SynchronizerData;
+
+public class CenterBehavior : MonoBehaviour {
+
+    private BeatObserver beatObserver;
+    private int beatCounter;
+
+
+    void Start()
+    {
+        beatObserver = GetComponent<BeatObserver>();
+        beatCounter = 0;
+    }
+
+    void Update()
+    {
+        if ((beatObserver.beatMask & BeatType.OnBeat) == BeatType.OnBeat)
+        {
+            //This will check for quarter beats for now, can add animations or some sort to the rune in the middle here!
+        }
+    }
+}
--- RuneForge/Assets/Minigames/Rhythm/Mapping/BeatmappingScript.cs
+++ RuneForge/Assets/Minigames/Rhythm/Mapping/BeatmappingScript.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using SynchronizerData;
 
 public class BeatmappingScript : MonoBehaviour {
 
@@ -9,26 +10,40 @@ public class BeatmappingScript : MonoBehaviour {
     private bool written = false;
     public List<float> timeList;
     public string songName;
+    float currentBeat;
+
+    private BeatObserver beatObserver;
+    private int beatCounter;
 
     void Start()
     {
         //writing stuff
         timeList = new List<float>();
+        beatObserver = GetComponent<BeatObserver>();
+        beatCounter = 0;
     }
 
     void Update()
     {
         //ALL WRITING STUFF
+        if ((beatObserver.beatMask & BeatType.OnBeat) == BeatType.OnBeat)
+            currentBeat = Time.time;
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log(Time.time);
-            timeList.Add(Time.time);
+            if (Time.time - currentBeat < 0)
+                timeList.Add(currentBeat);
+            else
+                timeList.Add(Time.time);
         }
 
         if (!GameObject.Find("AudioSource").GetComponent<AudioSource>().isPlaying)
         {
-            foreach (float beat in timeList)
-                writer += beat.ToString() + " ";
+            foreach (float map in timeList)
+            {                
+                writer += map.ToString() + " ";                
+            }
+
             if (!written)
             {
                 System.IO.File.WriteAllText("C:/Users/DavidTruong/Desktop/RuneForge/RuneForge/Assets/Resources/Beatmaps/" + songName + ".txt", writer + "\n");
