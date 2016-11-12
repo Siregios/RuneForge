457819ae572d886527a375363ab9c102f7cad913
pgtruong <petergtruong@gmail.com>
2016-11-12 13:41:53 -0800

    Sorting Game Pt2
    Need to fix rotation of instantiated objects.

--- RuneForge/Assets/Minigames/SortGame/SortGameManager.cs
+++ RuneForge/Assets/Minigames/SortGame/SortGameManager.cs
@@ -4,14 +4,38 @@ using System.Collections;
 public class SortGameManager : MonoBehaviour {
 
     public GameObject[] runes;
+    public GameObject[] bubbles;
     bool grabbed = false;
     GameObject drag;
+    public int currentSpawn = 0;
+    Vector3 oldPos;
+    bool notDone = true;
+    float timeToSpawn = 3f;
+    float time;
 
 	void Start () {
-	
+        time = timeToSpawn;
 	}
 		
 	void Update () {
+        time -= Time.deltaTime;
+
+        //Check if any characters are requesting an item.
+        if (currentSpawn < 3 && time <= 0)
+        {           
+            int randomInt = Random.Range(0, 3);
+            if (bubbles[randomInt].activeSelf == false)
+            {
+                bubbles[randomInt].SetActive(true);
+                time = timeToSpawn;
+                int randomRune = Random.Range(0, 4);
+                GameObject spawnedRune = (GameObject) Instantiate(runes[randomRune], bubbles[randomInt].transform.position, Quaternion.identity);
+                spawnedRune.transform.parent = bubbles[randomInt].transform;
+                
+            }                            
+        }
+
+        //On click, check if you clicked on rune by tag and if so then grab it.
 	    if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);            
@@ -22,24 +46,35 @@ public class SortGameManager : MonoBehaviour {
                 {
                     grabbed = true;
                     drag = hit.collider.gameObject;
+                    oldPos = hit.collider.gameObject.transform.position;
                 }
 
             }
-
+            //Makes cursor invisible
             if (grabbed)
                 Cursor.visible = false;
         }
 
+        //While left click down, drag object around.
         if (Input.GetMouseButton(0))
         {
-            Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            drag.transform.position = new Vector3(targetPosition.x, targetPosition.y, 0f);
+            if (grabbed)
+            {
+                Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                drag.transform.position = new Vector3(targetPosition.x, targetPosition.y, 0f);
+            }
         }
 
-            if (Input.GetMouseButtonUp(0))
+        //On left click let go, object is donezoes.
+        if (Input.GetMouseButtonUp(0))
         {
-            Cursor.visible = true;
-            grabbed = false;
+            if (grabbed)
+            {
+                drag.transform.position = oldPos;
+                drag = null;
+                Cursor.visible = true;
+                grabbed = false;
+            }
         }
 	}
 }
