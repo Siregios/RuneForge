321826d49a60608501c431726776ee079cbf85b7
pgtruong <petergtruong@gmail.com>
2016-11-12 16:21:48 -0800

    Sorting Game Pt3
    Basic prototype made, just need to add score and increasing difficulty.
    Afterwards its just polish with sounds, sprites, and animations.

---
+++ RuneForge/Assets/Minigames/SortGame/RuneCollision.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RuneCollision : MonoBehaviour {
+
+    public GameObject GameManager;
+    SortGameManager managerScript;
+	// Use this for initialization
+	void Start () {
+        managerScript = GameManager.GetComponent<SortGameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check to see if correct collision with right rune
+        if (gameObject.tag == other.gameObject.tag)
+        {
+            //Reset character
+            foreach (Transform child in other.gameObject.transform.parent.GetComponentsInChildren<Transform>())
+            {                
+                if (child.gameObject.tag == "Character")
+                {                   
+                    child.transform.position = new Vector3(0, managerScript.charY, 0);
+                }
+            }
+            //Allocate score and subtract current spawn, remove children and destroy rune object
+            managerScript.score += 10;
+            managerScript.currentSpawn--;            
+            other.gameObject.transform.parent.gameObject.SetActive(false);
+            Destroy(other.gameObject);
+            other.gameObject.transform.parent.DetachChildren();
+            managerScript.resetPosition();
+        }
+    }
+}
--- RuneForge/Assets/Minigames/SortGame/SortGameManager.cs
+++ RuneForge/Assets/Minigames/SortGame/SortGameManager.cs
@@ -2,79 +2,144 @@
 using System.Collections;
 
 public class SortGameManager : MonoBehaviour {
-
+    
+    //Draggable runes and bubble spawns
     public GameObject[] runes;
     public GameObject[] bubbles;
+    public GameObject[] characters;
+    public int charY = -7;
+
+    //Check if object is clicked on and allow drag
     bool grabbed = false;
     GameObject drag;
+
+    //How many are currently spawned
     public int currentSpawn = 0;
+
+    //resets drag position
     Vector3 oldPos;
-    bool notDone = true;
+    
+    //Intervals to how fast bubbles will spawn    
     float timeToSpawn = 3f;
     float time;
+    public int score;
 
 	void Start () {
-        time = timeToSpawn;
+        time = 0;
 	}
 		
 	void Update () {
-        time -= Time.deltaTime;
-
         //Check if any characters are requesting an item.
         if (currentSpawn < 3 && time <= 0)
-        {           
-            int randomInt = Random.Range(0, 3);
-            if (bubbles[randomInt].activeSelf == false)
-            {
-                bubbles[randomInt].SetActive(true);
-                time = timeToSpawn;
-                int randomRune = Random.Range(0, 4);
-                GameObject spawnedRune = (GameObject) Instantiate(runes[randomRune], bubbles[randomInt].transform.position, Quaternion.identity);
-                spawnedRune.transform.parent = bubbles[randomInt].transform;
-                
-            }                            
+        {
+            spawnTarget();
         }
+
+        //Otherwise subtract timer while we still needa spawn more stuff OKAY EFREN JESUS I WAS JUST TRYING TO EXPLAIN THIS PART TO YOU OKAY?
+        if (currentSpawn < 3)
+        {
+            time -= Time.deltaTime;
+        }
+
+
 
         //On click, check if you clicked on rune by tag and if so then grab it.
 	    if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);            
-            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
-            if (hit != null && hit.collider != null)
-            {
-                if (hit.collider.tag == "Red" || hit.collider.tag == "Yellow" || hit.collider.tag == "Blue" || hit.collider.tag == "Green")
-                {
-                    grabbed = true;
-                    drag = hit.collider.gameObject;
-                    oldPos = hit.collider.gameObject.transform.position;
-                }
-
-            }
-            //Makes cursor invisible
-            if (grabbed)
-                Cursor.visible = false;
+            checkClickedObject();
         }
 
         //While left click down, drag object around.
         if (Input.GetMouseButton(0))
         {
-            if (grabbed)
-            {
-                Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                drag.transform.position = new Vector3(targetPosition.x, targetPosition.y, 0f);
-            }
+            dragObject();        
         }
 
         //On left click let go, object is donezoes.
         if (Input.GetMouseButtonUp(0))
         {
-            if (grabbed)
-            {
-                drag.transform.position = oldPos;
-                drag = null;
-                Cursor.visible = true;
-                grabbed = false;
-            }
+            resetPosition();
         }
 	}
+
+    //Functions for dragging objects
+
+    void checkClickedObject()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+        if (hit != false && hit.collider != null)
+        {
+            if (hit.collider.tag == "Red" || hit.collider.tag == "Yellow" || hit.collider.tag == "Blue" || hit.collider.tag == "Green")
+            {
+                grabbed = true;
+                drag = hit.collider.gameObject;
+                oldPos = hit.collider.gameObject.transform.position;
+            }
+
+        }
+        //Makes cursor invisible
+        if (grabbed)
+            Cursor.visible = false;
+    }
+
+    void dragObject()
+    {
+        if (grabbed)
+        {
+            Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            drag.transform.position = new Vector3(targetPosition.x, targetPosition.y, 0f);
+        }
+    }
+
+    public void resetPosition()
+    {
+        if (grabbed)
+        {
+            drag.transform.position = oldPos;
+            drag = null;
+            Cursor.visible = true;
+            grabbed = false;
+        }
+    }
+
+    public void spawnTarget()
+    {
+        int randomInt = 0;
+        bool check = true;
+        while (check)
+        {        
+            randomInt = Random.Range(0, 3);
+            if (bubbles[randomInt].activeSelf == false)
+            {
+                check = false;
+                bubbles[randomInt].SetActive(true);
+                time = timeToSpawn;
+                int randomRune = Random.Range(0, 4);
+                GameObject spawnedRune = (GameObject)Instantiate(runes[randomRune], bubbles[randomInt].transform.position, Quaternion.identity);
+                spawnedRune.transform.parent = bubbles[randomInt].transform;
+                currentSpawn++;
+
+                //Placeholder to fit the object in bubble
+                spawnedRune.transform.position = new Vector3(spawnedRune.transform.position.x, spawnedRune.transform.position.y + 0.5f, spawnedRune.transform.position.z);
+            }
+        }
+        spawnCharacter(randomInt);        
+    }
+
+    void spawnCharacter(int i)
+    {
+        bool check = true;
+        while (check)
+        {
+            int randomChar = Random.Range(0, 3);
+            if (characters[randomChar].transform.position.y == charY)
+            {
+                check = false;
+                characters[randomChar].transform.position = new Vector3(bubbles[i].transform.position.x, charY, 0);
+                characters[randomChar].transform.position = Vector3.MoveTowards(characters[randomChar].transform.position, new Vector3(bubbles[i].transform.position.x, -4, 0), 10);
+                characters[randomChar].transform.parent = bubbles[i].transform;
+            }
+        }
+    }
 }
