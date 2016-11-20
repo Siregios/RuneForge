78657de09319a23482be102a3bc9b9cc38320967
Edwin Ho <eaho1@uci.edu>
2016-11-19 18:12:17 -0800

    Started Golem Fetch

---
+++ RuneForge/Assets/Minigames/GolemFetch/Cell.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Cell : MonoBehaviour {
+    public enum CellOrientation
+    {
+        NONE,
+        BOTTOM_LEFT,
+        BOTTOM_RIGHT,
+        TOP_RIGHT,
+        TOP_LEFT
+    }
+    int orientationInt = 0;
+
+    public CellOrientation orientation
+    {
+        get { return (CellOrientation)orientationInt; }
+    }
+    public int x
+    {
+        get { return Mathf.FloorToInt(this.transform.position.x); }
+    }
+    public int y
+    {
+        get { return Mathf.FloorToInt(this.transform.position.y); }
+    }
+
+    void OnMouseDown()
+    {
+        orientationInt = (orientationInt + 1) % 5;
+        Debug.LogFormat("Clicked me: ({0}, {1}) - {2}", x, y, orientation.ToString());
+    }
+}
---
+++ RuneForge/Assets/Minigames/GolemFetch/GolemFetchManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GolemFetchManager : MonoBehaviour {
+    public enum Direction
+    {
+        LEFT,
+        RIGHT,
+        DOWN,
+        UP
+    }
+
+    public GameObject cellObject;
+    public int gridSize = 15;
+    [HideInInspector]
+    public Direction entranceWall;
+    public Vector2 startCell;
+
+    public List<List<Cell>> grid = new List<List<Cell>>();
+
+    void Awake()
+    {
+        Camera.main.orthographicSize = (float)gridSize / 2;
+        Camera.main.transform.position = new Vector3((float)gridSize / 2, (float)gridSize / 2, -10);
+
+        entranceWall = PickEntranceWall();
+
+        for (int c = 0; c < gridSize; c++)
+        {
+            grid.Add(new List<Cell>());
+            for (int r = 0; r < gridSize; r++)
+            {
+                GameObject newCellObject = (GameObject)Instantiate(cellObject, new Vector2(c, r), Quaternion.identity);
+                Cell newCell = newCellObject.GetComponent<Cell>();
+                grid[c].Add(newCell);
+            }
+        }
+    }
+
+    Direction PickEntranceWall()
+    {
+        return RandomEnum<Direction>();
+    }
+
+    public T RandomEnum<T>()
+    {
+        var values = Enum.GetValues(typeof(T));
+        return (T)values.GetValue(UnityEngine.Random.Range(0, values.Length));
+    }
+}
