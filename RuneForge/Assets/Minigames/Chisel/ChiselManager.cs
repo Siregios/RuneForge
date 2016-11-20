f10da795e9ca8727a2ab5eca3a9eb1b5648d462c
Christopher Peterson <cjpeter1@uci.edu>
2016-11-19 16:30:56 -0800

    Chisel Part 1 and Shopkeeper

---
+++ RuneForge/Assets/Minigames/Chisel/ChiselManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ChiselManager : MonoBehaviour {
+
+    public int score = 0;
+    public Text scoreText;
+    public GameObject map;
+
+    int level = 0;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+      
+	}
+
+    void LateUpdate()
+    {
+        if (map != null && map.transform.childCount == 0)
+        {
+            score += 10;
+            level++;
+            Destroy(map);
+        }
+        scoreText.text = "Score: " + score;
+    }
+}
---
+++ RuneForge/Assets/Minigames/Chisel/Target.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+    public ChiselManager manager;
+
+	void OnMouseDown()
+    {
+        manager.score += 10;
+        Destroy(this.gameObject);
+
+    }
+}
