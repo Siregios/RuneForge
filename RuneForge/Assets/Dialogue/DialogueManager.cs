910c932ea8769746357bf06c0561ac309d9f5184
Christopher Peterson <cjpeter1@uci.edu>
2016-12-26 15:48:02 -0800

    Dialogue Port Part 1
    Press F to start dialogue, C to continue dialogue

---
+++ RuneForge/Assets/Dialogue/DialogueManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+    public GameObject dialogueUI;
+    public DialogueHandler dialogueScript;
+
+    void Start()
+    {
+        dialogueScript = GameObject.FindGameObjectWithTag("DialogueUI").GetComponent<DialogueHandler>();
+        dialogueUI = dialogueScript.dialogueUI;
+        
+        dialogueUI.SetActive(false);
+    }
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            dialogueUI.SetActive(true);
+            dialogueScript.GetComponent<DialogueHandler>().LoadTextAsset(0);
+            dialogueScript.enabled = true;
+            dialogueScript.SetBackground("Temp");
+        }
+    }
+}
