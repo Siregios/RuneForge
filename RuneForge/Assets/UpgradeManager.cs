0ee3597591fe963dafd334f0fe7a55ddb3459519
Christopher Peterson <cjpeter1@uci.edu>
2017-02-04 16:27:24 -0800

    Upgrades Pt1

---
+++ RuneForge/Assets/UpgradeManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour {
+    bool omni = false;      //bool that checks if you can by all upgrades at one level
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
