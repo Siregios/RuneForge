e14e0a40e2f6333f8b1fa8dec20af7dc8a5461fe
pgtruong <petergtruong@gmail.com>
2017-02-11 14:19:12 -0800

    Result Screen Feedback Pt1
    Need to fully fix fill

--- RuneForge/Assets/Minigames/MinigameUI/ResultScreen.cs
+++ RuneForge/Assets/Minigames/MinigameUI/ResultScreen.cs
@@ -4,18 +4,25 @@ using UnityEngine.UI;
 
 public class ResultScreen : MonoBehaviour {
 
-    CanvasGroup fade;
+    public Text result;
+    public Image product;
+    public GameObject score;
+    public GameObject scoreFill;
+    public GameObject progress;
+    public Button done;
     float time = 1.5f;
-    float transition = 1.5f;
+    int minigameScore, totalScore;
+    float st = 500, hq = 1000, mc = 1500;
+    Image bronze, silver, gold;
+    //float transition = 1.5f;
 
     public string nextScene = "Workshop";
 
     void Start() {
-        //Fade
-        fade = GetComponent<CanvasGroup>();
-        
-        //Start to fade into result screen
-        StartCoroutine("FadeIn");
+        //set fills
+        bronze = scoreFill.transform.FindChild("Standard").GetComponent<Image>();
+        silver = scoreFill.transform.FindChild("High Quality").GetComponent<Image>();
+        gold = scoreFill.transform.FindChild("Master Craft").GetComponent<Image>();
 
         //Find final score text
         //string scoreText = GameObject.Find("Score").transform.Find("ScoreText").GetComponent<Text>().text;
@@ -24,8 +31,17 @@ public class ResultScreen : MonoBehaviour {
         //Update the work order with the score
         foreach (WorkOrder order in MasterGameManager.instance.workOrderManager.currentWorkOrders)
         {
-            order.UpdateOrder(MasterGameManager.instance.sceneManager.currentScene, GameObject.Find("Score").GetComponent<Score>().score);
-            
+            order.score = 1000;
+            bronze.fillAmount = Mathf.Clamp(order.score / st, 0, 1);
+            silver.fillAmount = Mathf.Clamp(order.score / hq, 0, 1);
+            gold.fillAmount = Mathf.Clamp(order.score / mc - hq, 0, 1);
+            minigameScore = GameObject.Find("Score").GetComponent<Score>().score;
+            order.UpdateOrder(MasterGameManager.instance.sceneManager.currentScene, minigameScore);
+            totalScore = order.score;   
+
+            //Start to fade into result screen
+            StartCoroutine(FadeResultText());
+
             //If complete, show work order score and rune completed.
             if (order.isComplete)
             {
@@ -51,17 +67,53 @@ public class ResultScreen : MonoBehaviour {
         }
     }
 
-    IEnumerator FadeIn()
+    IEnumerator FadeResultText()
     {
         Time.timeScale = 0;
-        while (fade.alpha <= 1)
+        while (GetComponent<Image>().color.a <= 1)
         {
-            fade.alpha += Time.unscaledDeltaTime / time;
-            yield return null;
+            Color temp = GetComponent<Image>().color;        
+            temp.a += Time.unscaledDeltaTime / time;
+            GetComponent<Image>().color = temp;
+            temp = result.color;
+            temp.a += Time.unscaledDeltaTime / time;
+            result.color = temp;
+            yield return new WaitForEndOfFrame();
         }
-        Time.timeScale = 1;
+        StartCoroutine(FadeProduct());
     }
 
+    IEnumerator FadeProduct()
+    {
+        while (product.color.a <= 1)
+        {
+            Color temp = product.color;
+            temp.a += Time.unscaledDeltaTime / time;
+            product.color = temp;
+            yield return new WaitForEndOfFrame();
+        }
+        StartCoroutine(FadeScore());
+    }
+
+    IEnumerator FadeScore()
+    {
+        while (score.GetComponent<CanvasGroup>().alpha < 1)
+        {
+            score.GetComponent<CanvasGroup>().alpha += Time.unscaledDeltaTime / time;
+            scoreFill.GetComponent<CanvasGroup>().alpha = score.GetComponent<CanvasGroup>().alpha;
+            yield return new WaitForEndOfFrame();
+        }
+        StartCoroutine(FadeScoreFill());
+    }
+
+    IEnumerator FadeScoreFill()
+    {
+        while (moveFillAmount())
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+    }
     //IEnumerator FadeOut()    
     //{
     //    Time.timeScale = 0;
@@ -83,6 +135,34 @@ public class ResultScreen : MonoBehaviour {
     //    Debug.Log("true");
     //    MasterGameManager.instance.sceneManager.LoadScene(nextScene);
     //}
+
+    bool moveFillAmount()
+    {
+        if (bronze.fillAmount < Mathf.Clamp((float)totalScore / st, 0f, 1f))
+        {
+            bronze.fillAmount = Mathf.Lerp(bronze.fillAmount, Mathf.Clamp((float)totalScore / st, 0f, 1f), Time.unscaledDeltaTime * 3);
+            if (1 - bronze.fillAmount <= 0.015f)           
+                bronze.fillAmount = 1;
+            
+        }
+        else if (silver.fillAmount < Mathf.Clamp((float)totalScore / hq, 0f, 1f))
+        {
+            silver.fillAmount = Mathf.Lerp(silver.fillAmount, Mathf.Clamp((float)totalScore / hq, 0f, 1f), Time.unscaledDeltaTime * 3);
+            if (1 - silver.fillAmount <= 0.015f)
+                silver.fillAmount = 1;
+        }
+        else if (gold.fillAmount < Mathf.Clamp((float)totalScore / mc, 0f, 1f))
+        {
+            gold.fillAmount = Mathf.Lerp(gold.fillAmount, Mathf.Clamp((float)totalScore / mc, 0f, 1f), Time.unscaledDeltaTime * 3);
+            if (1 - gold.fillAmount <= 0.015f)
+                gold.fillAmount = 1;
+        }
+        else
+        {
+            return false;
+        }
+        return true;
+    }
 
     public  void LoadScene()
     {
