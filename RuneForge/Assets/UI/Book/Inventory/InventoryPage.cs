7609ac52b3fa1ae1e15b3aabe393e973bde19fa9
Edwin Ho <eaho555@gmail.com>
2017-03-07 12:26:29 -0800

    Inventory UI (nearly?) completed

---
+++ RuneForge/Assets/UI/Book/Inventory/InventoryPage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryPage : MonoBehaviour {
+    private InventoryUIManager inventoryManager;
+    private Item item;
+
+    public Text itemName;
+    public Image itemIcon;
+    public AttrBarGroup attributeBars;
+
+    public void SetItem(Item item)
+    {
+        this.item = item;
+        itemName.text = item.name;
+        itemIcon.color = Color.white;
+        itemIcon.sprite = item.icon;
+
+        SetAttributeBars(item);
+    }
+
+    public void Clear()
+    {
+        itemName.text = "";
+        itemIcon.color = Color.clear;
+    }
+
+    void SetAttributeBars(Item item)
+    {
+        foreach (var kvp in item.providedAttributes)
+        {
+            attributeBars.SetBar(kvp.Key, kvp.Value);
+            attributeBars.SetText(kvp.Key, kvp.Value.ToString());
+        }
+    }
+}
---
+++ RuneForge/Assets/UI/Book/Inventory/InventoryUIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUIManager : MonoBehaviour {
+    Item selectedItem;
+
+    public InventoryPage inventoryPage;
+    public ItemListUI inventoryItemList;
+
+    void Awake()
+    {
+        inventoryItemList.AddButtonFunction(ClickInventoryItemButton);
+    }
+
+    void ClickInventoryItemButton(Item item)
+    {
+        inventoryPage.SetItem(item);
+    }
+}
