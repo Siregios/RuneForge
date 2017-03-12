f31d9a762e35fc945b5b3c23b590aa1f7d16f11b
Edwin Ho <eaho555@gmail.com>
2017-03-11 16:27:15 -0800

    Begun adding calendar to book

--- RuneForge/Assets/UI/MenuBar/MenuBar.cs
+++ RuneForge/Assets/UI/MenuBar/MenuBar.cs
@@ -6,12 +6,12 @@ public class MenuBar : MonoBehaviour {
     public Text moneyText;
     public Text dayText;
     public Text actionText;
-    public Button endDayButton;
+    //public Button endDayButton;
     public Image clockHand;
 
     void Awake()
     {
-        endDayButton.gameObject.SetActive(false);
+        //endDayButton.gameObject.SetActive(false);
     }
 
     void Update()
@@ -22,14 +22,14 @@ public class MenuBar : MonoBehaviour {
         int actionCount = MasterGameManager.instance.actionClock.ActionCount;
         actionText.text = string.Format("Actions: {0}/{1}", actionCount, MasterGameManager.instance.actionClock.ActionsPerDay);
         clockHand.rectTransform.rotation = Quaternion.Euler(0, 0, (float)MasterGameManager.instance.actionClock.ActionCount / MasterGameManager.instance.actionClock.ActionsPerDay * 360);
-        if (actionCount == 0)
-        {
-            endDayButton.gameObject.SetActive(true);
-        }
-        else
-        {
-            endDayButton.gameObject.SetActive(false);
-        }
+        //if (actionCount == 0)
+        //{
+        //    endDayButton.gameObject.SetActive(true);
+        //}
+        //else
+        //{
+        //    endDayButton.gameObject.SetActive(false);
+        //}
 
         if (Input.GetKeyDown(KeyCode.E))
         {
